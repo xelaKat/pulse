523a7097b57b8905230586467f080dd344384d42
xelaKat <ahuang28@nobles.edu>
2026-07-14 19:36:28 -0400

    show data

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY; //import api key
 document.querySelector("#app").innerHTML = "<p>loading...</p>"; //show something while loading
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
-    console.log(data);
+    document.querySelector("#app").innerHTML = ` 
+    <h1> ${data.title} </h1>
+    <img src = "${data.url}" />
+    <p> ${data.explanation} </p>
+    
+    `;
 }) //fetches data, handles response, uses the data
 
 //date selector
