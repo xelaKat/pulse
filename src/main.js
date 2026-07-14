04de17a16db7f0ef05cfb53f1f1a15f35a9fe89e
xelaKat <ahuang28@nobles.edu>
2026-07-14 19:27:54 -0400

    update main.js to handle api and update index.html

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
+//api code
+const API_KEY = import.meta.env.VITE_NASA_API_KEY; //import api key
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>"; //show something while loading
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
+    console.log(data);
+}) //fetches data, handles response, uses the data
+
+//date selector
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+
+
