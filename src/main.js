bbc0e8d96edd019003d83e854bff4264f4a89ceb
xelaKat <ahuang28@nobles.edu>
2026-07-14 19:45:43 -0400

    formatting

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ const API_KEY = import.meta.env.VITE_NASA_API_KEY; //import api key
 document.querySelector("#app").innerHTML = "<p>loading...</p>"; //show something while loading
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
-    document.querySelector("#app").innerHTML = ` 
-    <h1> ${data.title} </h1>
-    <img src = "${data.url}" />
-    <p> ${data.explanation} </p>
-    
-    `;
+  let media;
+    if (data.media_type === "image") {
+        media = `<img src="${data.url}" width="500" height="400" />`;
+    } else {
+        media = `<video src="${data.url}" controls></video>`;
+    }
+
+  document.querySelector("#app").innerHTML = ` 
+  <h1> ${data.title} </h1>
+  ${media}
+  <p> ${data.explanation} </p>  
+  `;
 }) //fetches data, handles response, uses the data
 
 //date selector
