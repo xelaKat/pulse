de621913b95d7eecb084ca58fb19d90f1d32a240
xelaKat <ahuang28@nobles.edu>
2026-07-14 20:08:52 -0400

    more testing + fixing css

--- src/main.js
+++ src/main.js
@@ -1,34 +1,13 @@
+//date selector
+const date = document.querySelector("#datepicker").value;
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
+
 //api code
 const API_KEY = import.meta.env.VITE_NASA_API_KEY; //import api key
 
 document.querySelector("#app").innerHTML = "<p>loading...</p>"; //show something while loading
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
-  let media;
-    if (data.media_type === "image") {
-        media = `<img src="${data.url}" width="500" height="400" />`;
-    } 
-    else if (data.url.includes("youtube")) {
-      media = `<p> sorry, this is supposed to be a youtube video, but i don't know how to embed those :( </p>`
-    }
-    else {
-        media = `<video src="https://www.youtube.com/watch?v=qqbsvcq3z4k&t=1009s" controls></video>`;
-    }
-
-  document.querySelector("#app").innerHTML = ` 
-  <h1> ${data.title} </h1>
-  ${media}
-  <p> ${data.explanation} </p>  
-  `;
-}) //fetches data, handles response, uses the data
-.catch(err => {
-  document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
-}); //shows error
-
-
-//date selector
-const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`).then(response => response.json()).then(data => {
   let media;
     if (data.media_type === "image") {
         media = `<img src="${data.url}" width="500" height="400" />`;
