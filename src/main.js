e48d050a5230bfc57ac2b9ddd6839514a5164de2
xelaKat <ahuang28@nobles.edu>
2026-07-14 20:15:19 -0400

    fixed previous bugs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 //date selector
 const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
 
 //api code
 const API_KEY = import.meta.env.VITE_NASA_API_KEY; //import api key
 
 document.querySelector("#app").innerHTML = "<p>loading...</p>"; //show something while loading
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`).then(response => response.json()).then(data => {
   let media;
     if (data.media_type === "image") {
         media = `<img src="${data.url}" width="500" height="400" />`;
